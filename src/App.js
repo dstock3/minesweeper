b7248479988eee2ee7fd7e1ca96fbac5bce636ec
David Stockdale <dstock3@gmail.com>
2022-09-28 14:25:30 -0500

    Added Minesweeper component and corresponding css file. Adjusted respective imports.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+import Minesweeper from '../src/components/Minesweeper'
 import './App.css';
 
 function App() {
+  const [mineInst, setMineInst] = useState(0)
   return (
     <div className="App">
-
+      <Minesweeper mineInst={mineInst} setMineInst={setMineInst} key={new Date().getTime()}/>
     </div>
   );
 }
